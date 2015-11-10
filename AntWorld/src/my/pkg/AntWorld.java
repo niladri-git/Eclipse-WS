f8c1c9e3769893ead20e15b24af4a72da800cb7b
Niladri Datta <niladri-git@gmail.com>
2015-11-11 00:23:25 +0530

    Added external library log4j

--- AntWorld/src/my/pkg/AntWorld.java
+++ AntWorld/src/my/pkg/AntWorld.java
@@ -1,10 +1,13 @@
 package my.pkg;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+
 public class AntWorld {
+    static Logger logger = Logger.getLogger(AntWorld.class);
 
-	public static void main(String[] args) {
-		System.out.println("Hello Ant!!!");
-
-	}
-
+    public static void main(String[] args) {
+        BasicConfigurator.configure();
+        logger.info("Ant World");          // the old SysO-statement
+    }
 }
