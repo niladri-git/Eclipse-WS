a5224b69ca23d9de33523a9e1dd8eb6af08a9622
Niladri Datta <niladridatta@gmail.com>
2015-11-11 14:13:41 +0530

    Removed Log4J's configuration hardcoding by adding log4j.properties

--- HelloAnt/src/my/pkg/HelloAnt.java
+++ HelloAnt/src/my/pkg/HelloAnt.java
@@ -1,13 +1,11 @@
 package my.pkg;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.BasicConfigurator;
 
 public class HelloAnt {
     static Logger logger = Logger.getLogger(HelloAnt.class);
 
     public static void main(String[] args) {
-        BasicConfigurator.configure();
         logger.info("Hello Ant!!!");          // the old SysO-statement
     }
 }
